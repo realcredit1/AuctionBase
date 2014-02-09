3818e235101c9e2305e3c33c47e3034fe476e99a
lauramiron <laurabmiron@gmail.com>
2014-02-08 20:25:45 -0800

    Part 2: added foreign key constraints to create.sql

--- create.sql
+++ create.sql
@@ -1,4 +1,6 @@
-create table Item (itemID string PRIMARY KEY, name string, currently string, buy_price string, first_bid string, started string, ends string, userID string, description string);
+create table Item (itemID string PRIMARY KEY, name string, currently string, 
+	buy_price string, first_bid string, started string, ends string, 
+	userID string references User, description string);
 create table User (userID string PRIMARY KEY, rating string, location string, country string);
-create table Category (itemID string, category string, PRIMARY KEY (itemID, category));
-create table Bid (itemID string, userID string, time string, amount string, PRIMARY KEY (itemID,time));
+create table Category (itemID string references Item, category string, PRIMARY KEY (itemID, category));
+create table Bid (itemID string references Item, userID string references User, time string, amount string, PRIMARY KEY (itemID,time));
