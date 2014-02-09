d2f28b03519e3a2bca5a4396363cb06bf08605a5
lauramiron <laurabmiron@gmail.com>
2014-02-09 05:33:15 -0800

    Part 3: Starter files for part 3

---
+++ webfiles/sqlitedb.py
@@ -0,0 +1,76 @@
+import web
+
+db = web.database(dbn='sqlite',
+        db='[YOUR SQLite DATABASE FILENAME]' #TODO: add your SQLite database filename
+    )
+
+######################BEGIN HELPER METHODS######################
+
+# Enforce foreign key constraints
+# WARNING: DO NOT REMOVE THIS!
+def enforceForeignKey():
+    db.query('PRAGMA foreign_keys = ON')
+
+# initiates a transaction on the database
+def transaction():
+    return db.transaction()
+# Sample usage (in auctionbase.py):
+#
+# t = sqlitedb.transaction()
+# try:
+#     sqlitedb.query('[FIRST QUERY STATEMENT]')
+#     sqlitedb.query('[SECOND QUERY STATEMENT]')
+# except:
+#     t.rollback()
+#     raise
+# else:
+#     t.commit()
+#
+# check out http://webpy.org/cookbook/transactions for examples
+
+# returns the current time from your database
+def getTime():
+    # TODO: update the query string to match
+    # the correct column and table name in your database
+    query_string = 'select currenttime from Time'
+    results = query(query_string)
+    # alternatively: return results[0]['currenttime']
+    return results[0].currenttime # TODO: update this as well to match the
+                                  # column name
+
+# returns a single item specified by the Item's ID in the database
+# Note: if the `result' list is empty (i.e. there are no items for a
+# a given ID), this will throw an Exception!
+def getItemById(item_id):
+    # TODO: rewrite this method to catch the Exception in case `result' is empty
+    query_string = 'select * from Items where item_ID = $itemID'
+    result = query(query_string, {'itemID': item_id})
+    return result[0]
+
+# helper method to determine whether query result is empty
+# Sample use:
+# query_result = sqlitedb.query('select currenttime from Time')
+# if (sqlitedb.isResultEmpty(query_result)):
+#   print 'No results found'
+# else:
+#   .....
+#
+# NOTE: this will consume the first row in the table of results,
+# which means that data will no longer be available to you.
+# You must re-query in order to retrieve the full table of results
+def isResultEmpty(result):
+    try:
+        result[0]
+        return False
+    except:
+        return True
+
+# wrapper method around web.py's db.query method
+# check out http://webpy.org/cookbook/query for more info
+def query(query_string, vars = {}):
+    return db.query(query_string, vars)
+
+#####################END HELPER METHODS#####################
+
+#TODO: additional methods to interact with your database,
+# e.g. to update the current time
